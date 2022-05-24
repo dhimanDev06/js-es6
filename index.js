5281ffa60646cc14f3aeafda667849fb67abac39
Bhattacharya <dhiman.bhattacharya@capgemini.com>
2022-05-24 18:15:43 +0530

    new code

--- index.js
+++ index.js
@@ -20,3 +20,20 @@ let arr = [1,2,4,6,7,3];
 console.log(arr.at(0)); //1
 console.log(arr.at(-1)); //2
 // It is similar to the array[index] but is not able to be used on the -1 index as like as arrary[-1]
+
+
+// import fetch from 'node-fetch';
+
+const loadData = async () =>{
+  try {
+    const url = "https://jsonplaceholder.typicode.com/posts"
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+  }catch(err){
+    console.log(err);
+  }
+}
+
+//const data = await 
+loadData().then(res => console.log(res));
